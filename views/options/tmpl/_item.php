64bedec1b0378c9fbbd3460db54793d48a9dce7b
M. Drew LaMar <mdlama@wm.edu>
2023-06-15 14:55:32 -0400

    [fix] Fixes for supergroup resource record view
    (cherry picked from commit 6f22b63e3dd377a37ac525191a6d478165b192b8)

--- views/options/tmpl/_item.php
+++ views/options/tmpl/_item.php
@@ -9,4 +9,4 @@
 defined('_HZEXEC_') or die();
 
 ?>
-	<a href="<?php echo Route::url('index.php?option=' . $this->option . '&id=' . $this->publication->id . '&active=share&v=' . $this->publication->version_number . '&sharewith=' . strtolower($this->name)); ?>" title="<?php echo Lang::txt('PLG_PUBLICATION_SHARE_ON', ucfirst($this->name)); ?>" class="popup" rel="external"><span class="share_<?php echo strtolower($this->name);  ?>"><span><?php echo Lang::txt('PLG_PUBLICATIONS_SHARE_' . strtoupper($this->name)); ?></span></span></a>
+	<a href="<?php echo Route::url('index.php?option=' . $this->option . '&id=' . $this->publication->id . '&active=share&v=' . $this->publication->version_number . '&sharewith=' . strtolower($this->name)); ?>" title="<?php echo Lang::txt('PLG_PUBLICATION_SHARE_ON', ucfirst($this->name)); ?>" class="popup" rel="external" target="_blank"><span class="share_<?php echo strtolower($this->name);  ?>"><span><?php echo Lang::txt('PLG_PUBLICATIONS_SHARE_' . strtoupper($this->name)); ?></span></span></a>
